06e9268cc4593b896f043e3294cd4c5104bf635c
Shlok Rastogi <shlokrastogi2004@gmail.com>
2025-12-03 19:19:01 +0530

    Learn about react element, react component, JSX basics

---
+++ README.md
@@ -0,0 +1,18 @@
+# Learning React
+
+# Parcel
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm - wrriten in C++
+- Cashing - Faster Builds
+- Image Optimization
+- Minification
+- Bundling
+- Compressing
+- Consisitent Hashing
+- Differential Bundling - support older browsers
+- Diagnostic
+- Error Handling
+- HTTPs
+- Tree Shaking - remove unused code
---
+++ src/LearningReact.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+//React.createElement => ReactElement - JS object => HTMLElement(render)
+
+// React Element
+// const heading = React.createElement("h1", { id: "heading" }, "hello world !!");
+
+// JSX (JSX is not html in javascript but it is html like syntax)
+//JSX transpilled before it reaches the JS - Parcel - Babel
+// JSX => React.createElement
+
+// const jsxHeading = (
+//   <h1 id="jsxHeading" className="heading-types">
+//     Hello everyone. I'm JSX
+//   </h1>
+// );
+// const jsxHeading2 = (
+//   <h1 id="jsxHeading" className="heading-types">
+//     Hello everyone. I'm JSX
+//   </h1>
+// );
+
+// // React Functional Component
+// const HeadingComponent = () => {
+//   return <h1>Hello, I'm Functional Component</h1>;
+// };
+
+// //Other way of writing if function have only return value
+// const HeadingComponent2 = () => <h1>Hello, I'm Functional Component</h1>;
+
+// //Other way of writing
+// const HeadingComponent3 = () => (
+//   <h1 id="head">Hello, I'm Functional Component</h1>
+// );
+
+// //Component Composition (one component in other component)
+// const HeadingComponent4 = () => (
+//   <div id="container">
+//     <HeadingComponent3 />
+//     <HeadingComponent3></HeadingComponent3>
+//     <h1 id="head">Hello, I'm Functional Component</h1>
+//   </div>
+// );
+
+// const data = 100;
+
+// //One component or one element in other component and element
+// const HeadingComponent5 = () => (
+//   <div id="container1">
+//     {100 + 300}
+//     {data}
+
+//     <h1 id="head">Hello, I'm Functional Component</h1>
+//   </div>
+// );
+
+// //Function inside component
+// const HeadingComponent6 = () => (
+//   <div id="container">
+//     {HeadingComponent3()}
+//     <h1 id="head">Hello, I'm Functional Component</h1>
+//   </div>
+// );
+
+// //Root for react app and render it
+// const root = ReactDOM.createRoot(document.getElementById("root")!); //! IS NON NULL ASSERTION (because we already create root element in index.html)
+// root.render(<HeadingComponent6 />);
