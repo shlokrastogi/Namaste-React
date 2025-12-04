4d330ede075746c284f8e5c5aaa6eb0668393292
Shlok Rastogi <shlokrastogi2004@gmail.com>
2025-12-04 23:54:29 +0530

    fix errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,2 +1,48 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+/**
+ * Header
+ * -logo
+ * -nav-items
+ *
+ * Body
+ * -search
+ * restaurant-container
+ * restaurant-card
+ *
+ * Footer
+ * -copyright
+ * -links
+ * -address
+ */
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          src="https://i.ibb.co/TxCccr45/logo-fotor-20251204224342.png"
+          alt="logo"
+          className="logo"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Cart</li>
+          <li>Contact Us</li>
+          <li></li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return <div className="app"></div>;
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root")!);
+root.render(<Header />);
