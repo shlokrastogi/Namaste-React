e28b822d3dd79c72032bca72fea2f18c5d645c18
Shlok Rastogi <shlokrastogi2004@gmail.com>
2026-01-29 17:50:39 +0530

    fix the padding

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -7,7 +7,7 @@ import appStore from "../utils/appStore";
 const AppLayout = () => (
   <Provider store={appStore}>
     <div className="mb-4">
-      <div className="mb-40 sm:mb-24">
+      <div className="mb-40 sm:mb-28">
         <Header />
       </div>
       <Outlet />
